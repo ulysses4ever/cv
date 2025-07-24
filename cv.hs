fbfe192d6db76762c88af8325cc43334a0c68ef4
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-07-24 11:25:34 -0400

    add Config record

--- cv.hs
+++ cv.hs
@@ -1,23 +1,30 @@
 #!/usr/bin/env cabal
 {- cabal:
 build-depends: base
+              , aeson
               , ginger
               , text
+default-language: GHC2021
 -}
 
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 module Main where
 
+import Data.Aeson (toJSON, ToJSON)
 import Text.Ginger
 import Text.Ginger.GVal
+import Data.Either (fromRight)
+import GHC.Generics (Generic)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 
 main :: IO ()
 main = do
-  etemplate <- parseGinger resolver Nothing "{{ name }}"
+  etemplate <- parseGinger resolver Nothing "{{ cfg.name }}"
   let template = fromRight (error "Boo") etemplate
   Text.putStrLn $
     runGinger
@@ -27,4 +34,17 @@ main = do
 
 resolver name = pure Nothing
 
-context "name" = toGVal ("Artem" :: Text)
+context = \case
+  "cfg" ->  toGVal . toJSON $ defaultConfig
+
+data Config = MkConfig
+  { name :: Text
+  , email :: Text
+  }
+  deriving (Generic, ToJSON)
+
+defaultConfig :: Config
+defaultConfig = MkConfig
+  { name = "Artem"
+  , email = "a@pelenitsyn.top"
+  }
